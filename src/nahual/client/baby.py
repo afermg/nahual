d16d7345c97285fa0210f39de24a52663e514ffd
Alán F. Muñoz <afer.mg@gmail.com>
2025-09-15 17:46:57 -0400

    feat: find overlaps

--- src/nahual/client/baby.py
+++ src/nahual/client/baby.py
@@ -165,12 +165,7 @@ def run_sample(
     # dtype must be either uint8 or uint16
     img = rng.integers(2**8, size=(2, 80, 120, 1), dtype="uint8")
 
-    output = process_data(
-        img,
-        address,
-        session_id,
-        input_dimorder = "NYXZ"
-    )
+    output = process_data(img, address, session_id, input_dimorder="NYXZ")
 
     return output
 
@@ -179,7 +174,7 @@ def process_data(
     img: np.ndarray,
     address: str,
     session_id: str,
-    input_dimorder : str = "NZYX",
+    input_dimorder: str = "NZYX",
     extra_args=(("refine_outlines", ("", "true")), ("with_edgemasks", ("", "true"))),
 ) -> list[dict[str, np.ndarray]]:
     """Sends image data to a baby-phone server session for segmentation.
@@ -236,7 +231,7 @@ def process_data(
         img = ((img / 65536) * 256).astype(np.uint8)
 
     # Convert from the input format to NYXZ
-    reordered = reorder_dims(img, input_dimorder = input_dimorder, output_dimorder="NYXZ")
+    reordered = reorder_dims(img, input_dimorder=input_dimorder, output_dimorder="NYXZ")
     # Initiate a multipart-encoded request
     requests.post(
         f"{address}/segment?sessionid={session_id}",
@@ -267,9 +262,13 @@ def process_data(
     # Return using updated labels?
     return edgemasks_labels
 
+
 import numpy as np
 
-def reorder_dims(img: np.ndarray, input_dimorder: str, output_dimorder: str) -> np.ndarray:
+
+def reorder_dims(
+    img: np.ndarray, input_dimorder: str, output_dimorder: str
+) -> np.ndarray:
     """Reorders the dimensions of an array based on string specifications.
 
     Parameters
@@ -295,7 +294,156 @@ def reorder_dims(img: np.ndarray, input_dimorder: str, output_dimorder: str) -> 
     ValueError
         If dimension orders are invalid or incompatible with the array shape.
     """
-    assert sorted(input_dimorder) == sorted(output_dimorder), "The strings must be permutations of each other"
+    assert sorted(input_dimorder) == sorted(
+        output_dimorder
+    ), "The strings must be permutations of each other"
     axes_permutation = [input_dimorder.index(dim) for dim in output_dimorder]
 
     return np.transpose(img, axes_permutation)
+
+
+def get_data():
+    mask = np.array([[0, 1, 1, 1, 2, 2], [1, 0, 2, 3, 3, 1]])
+
+    return (mask, mask + 1)
+
+
+def find_overlap_from_ijv(ijv: np.ndarray, uniq, col: int) -> list[np.ndarray]:
+    """
+    Find ijv subsets with at least one value
+    "sandwiched" between two over `col`.
+    """
+    v_col = 2
+    argsort = ijv[:, col].argsort()
+    sorted_labels = ijv[argsort, v_col]
+
+    # Find the boundaries using a boolean array per label
+    vals = uniq[:, None] == sorted_labels
+    left = vals.argmax(axis=1)
+    right = vals[:, ::-1].argmax(axis=1)
+
+    # These "inbetween" pixels may not be homogeneous,
+    # that is why I am using lists
+    between = [ijv[argsort][start:end] for start, end in zip(left, (len(ijv) - right))]
+
+    arg_overlap = [np.where(x[:, v_col] != x_val)[0] for x, x_val in zip(between, uniq)]
+    overlap_ijv = [between[i][x] for i, x in enumerate(arg_overlap)]
+
+    return overlap_ijv
+
+
+def find_overlapping_pixels(ijv: np.ndarray) -> dict[int, np.ndarray]:
+    uniq = np.unique(ijv[:, v_col])
+    x_overlap = find_overlap_from_ijv(ijv, col=0, uniq=uniq)
+    y_overlap = find_overlap_from_ijv(ijv, col=1, uniq=uniq)
+
+    result =  {label: x[index_isin(x, y)[:,0]] for label, x, y in zip(uniq, x_overlap, y_overlap)}
+    return result
+
+
+def group_edgemasks(masks: list[np.ndarray]):
+    # Cells are considered overlapping if for any pixel (x_1,y_1) there is a pair of pixels (x_2,y_2), (x_3,y_3) belonging to a different mask
+    # where (x_2 < x_1 < x_3) and (y_2 < y_1 < y_3).
+    ijv = edgemasks_to_ijv(masks)
+
+
+def edgemasks_to_ijv(masks: list[np.ndarray]) -> np.ndarray:
+    """Convert edgemasks to an ijv matrix"""
+    lens = [len(x[0]) for x in masks]
+    ijv = np.zeros((np.sum(lens), 3), dtype=np.uint16)
+    position = np.zeros(len(lens) + 1, dtype=np.uint16)
+    position[1:] = np.cumsum(lens)
+    for v, (x, y) in enumerate(masks):
+        slc = slice(position[v], position[v + 1])
+        ijv[slc, 0] = x
+        ijv[slc, 1] = y
+        ijv[slc, 2] = v
+
+    return ijv
+
+
+# def edge_to_ijv():
+# def edge_to_nzyx():
+# """Convert a representation of edgemasks into a dense representation of overlapping
+# masks where the first dimension ensures no overlap.
+# """
+# 1.Check any overlap
+# 1.1 Min-on-max
+# 2. If exists, identify which sets of masks overlap
+# 3. Solve as a linear programming problem
+# 3.1 Find the smallest number of subgroups
+# 3.2 Sort by size
+# 3.3 Assign subgroup to all non-overlapping masks
+# 3.4
+
+
+def index_isin(x: np.ndarray, y: np.ndarray, i_dtype = {"names": ["i", "j", "v"], "formats": [np.uint16, np.uint16, np.uint16]}) -> np.ndarray:
+    """
+    Find those elements of x that are in y.
+
+    Both arrays must be arrays of integer indices,
+    such as (trap_id, cell_id).
+
+    NOTE: This is (AFAIK) the fastest way to do this.
+    This is more of a database-querying task but
+    it is very common that we ought to do it with numpy.
+    """
+    x = np.ascontiguousarray(x, dtype=np.int16)
+    y = np.ascontiguousarray(y, dtype=np.int16)
+    xv = x.view(i_dtype)
+    inboth = np.intersect1d(xv, y.view(i_dtype))
+    x_bool = np.isin(xv, inboth)
+    return x_bool
+
+
+# Old code
+# There may be a way to do this with broadcasting
+# but it eluded me
+# simple = np.array([0,2,1,3])
+# ijv = np.stack((simple,simple, (0,0,1,1))).T
+
+# vals = np.unique(ijv[:,2])
+# occurrences = vals[:,None] == ijv[:,2]
+
+# less_x = np.less_equal.outer(ijv[:,0],ijv[:,0])
+# greater_x = np.greater_equal.outer(ijv[:,0],ijv[:,0])
+# less_y = np.less_equal.outer(ijv[:,1],ijv[:,1])
+# greater_y = np.greater_equal.outer(ijv[:,1],ijv[:,1])
+
+# Places where the pixels sits between two others
+# TODO Fix: ensure the output corresponds to in-between
+# x = i, y = lower than i?, z = higher than i?
+# inbetween_x = (less_x[..., None] & greater_x[None])
+# correct = np.array([less_x[i][:,None] & greater_x[i] for i in range(len(less_x))])
+# inbetween_x = (less_x & greater_x[:,None]).transpose((2,0,1))
+# inbetween_y = (less_y & greater_y[:,None]).transpose((2,0,1))
+# Remove the ones from the same object (equal_v)
+# Remove the ones where the pixels belong to the same object
+# array([[,
+#         [ True, False, False, False],
+#         [ True, False, False, False],
+#         [ True, False, False, False]],
+
+#        [[False, False, False, False],
+#         [ True,  True, False, False],
+#         [ True,  True, False, False],
+#         [ True,  True, False, False]],
+
+#        [[False, False, False, False],
+#         [False, False, False, False],
+#         [ True,  True,  True, False],
+#         [ True,  True,  True, False]],
+
+#        [[False, False, False, False],
+#         [False, False, False, False],
+#         [False, False, False, False],
+#         [ True,  True,  True,  True]]])
+
+# inbetween_x = less_x & greater_x[:,None]
+# inbetween_y = less_y & greater_y[:,None]
+# Inbetween = inbetween_x & inbetween_y
+
+# # Filter where it is each object
+# overlaps = np.where(inbetween & equal_v)
+
+# return np.unique(ijv[overlalps,2])
